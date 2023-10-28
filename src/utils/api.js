63d1fdea72fc5af74d002f4d7abc51e00eaaf282
Prashantch15 <beingpisach@gmail.com>
2023-10-28 22:57:47 +0530

    update api key

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,9 +2,9 @@ import axios from "axios";
 const BASE_URL ="https://youtube138.p.rapidapi.com";
 
 const options = {
-   
+
     params: {
-     
+
       hl: 'en',
       gl: 'US'
     },
@@ -14,7 +14,21 @@ const options = {
     }
   };
 
-  export const fetchDataFromApi = async (url)=>{
-    const {data} =await axios.get(`${BASE_URL}/${url}`,options);
-    return data;
-  }
+// const options = {
+//   method: 'GET',
+//   url: 'https://youtube138.p.rapidapi.com/auto-complete/',
+//   params: {
+//     q: 'desp',
+//     hl: 'en',
+//     gl: 'US'
+//   },
+//   headers: {
+//     'X-RapidAPI-Key': '068f075a4emsh80bb08fd95b4c86p1d7d1fjsn0882f3d2c6ae',
+//     'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
+//   }
+// };
+
+export const fetchDataFromApi = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+  return data;
+}
